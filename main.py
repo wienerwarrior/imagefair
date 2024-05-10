8c9853f543cf48e4a3fafcc95ac167586f676299
Yosh Nebe <sauteboy@gmail.com>
2024-05-10 15:18:15 -0700

    adding routes

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+import requests
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
+API_KEY = os.getenv('GOOGLE_CUSTOM_SEARCH_API')
+CSE_ID = os.getenv('GOOGLE_CSE_ID')
+
+
+@app.route("/search", methods=["GET"])
+def image_search():
     return "<p>Hello, World!</p>"
